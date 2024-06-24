38c6d04c05b7c4ebc70dd477b8ea6af65433d649
Tom Sasson <toms752384@gmail.com>
2024-06-24 10:14:13 +0300

    commit

---
+++ models/users.js
@@ -0,0 +1,7 @@
+//create users list
+const userList = [];
+
+//add to users
+
+//export users
+export default userList;
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+//create app and port
+const app = express();
+const port = 80;
+
+//middleware for parsing JSON bodies
+app.use(bodyParser.json());
+
+//CORS configuration
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow requests from this origin
+  credentials: true // Allow credentials (cookies)
+}));
+
+//handle preflight requests for all routes
+app.options('*', cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+
+//middleware for parsing cookies
+app.use(cookieParser());
+
+//start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
